cb775db0dedb570bdda70a20d5e39067d14a20a5
Joey Agpawa <joey@icvdm.com>
2018-05-18 23:53:18 -0500

    Added repo link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,10 @@ module.exports = {
     head: [],
     themeConfig: {
         lastUpdated: "Last Updated",
+        repo: 'ICVDM/docs-v3plus',
+        docsDir: 'docs',
+        editLinks: true,
+        editLinkText: 'Help improve this page!',
         sidebarDepth: 2,
         navbar: false,
         sidebar: [
